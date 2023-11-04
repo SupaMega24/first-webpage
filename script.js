9a729584d257fb063d2be32f3cd4dbea229c270d
noobslearn <138974329+SupaMega24@users.noreply.github.com>
2023-11-04 16:41:19 +0900

    Add modal and media query for mobile devices

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// Get the modal element
-var modal = document.getElementById("myModal");
-
-// Get the image inside the card
-var img = document.querySelector(".card-image");
-
-// Get the <span> element that closes the modal
-var span = document.querySelector(".close");
-
-// When the user clicks the image, open the modal
-img.onclick = function() {
-    modal.style.display = "block";
+      
+function openCatModal(photoUrl) {
+    
+    const catPhotoElement = document.getElementById('catPhoto');
+    const image = new Image();
+    image.src = photoUrl;
+    image.onload = function() {
+        catPhotoElement.innerHTML = ''; // Clear any previous content
+        catPhotoElement.appendChild(image);
+    };
+    
+    document.getElementById('catModal').style.display = 'flex';
+    
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+function closeCatModal() {
+    document.getElementById('catModal').style.display = 'none';            
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+    
